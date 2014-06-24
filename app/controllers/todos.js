5d92a9c02477cd36e2257223cfbca4615ed9c9bf
James Herdman <james.herdman@me.com>
2014-06-24 19:20:34 -0400

    All Done!

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
+var computed = Ember.computed;
+
 export default Ember.ArrayController.extend({
   remaining: function () {
     return this.filterBy('isCompleted', false).get('length');
@@ -9,7 +11,29 @@ export default Ember.ArrayController.extend({
     return this.get('remaining') === 1 ? 'todo' : 'todos';
   }.property('remaining'),
 
+  completed: function () {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
+
+  hasCompleted: computed.gt('completed', 0),
+
+  allAreDone: function (key, value) {
+    if (value === undefined) {
+      return !!this.get('length') && this.isEvery('isCompleted');
+    } else {
+      this.setEach('isCompleted', value);
+      this.invoke('save');
+      return value;
+    }
+  }.property('@each.isCompleted'),
+
   actions: {
+    clearCompleted: function () {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
+    },
+
     createTodo: function () {
       var title = this.get('newTitle');
       if (!title.trim()) { return; }
